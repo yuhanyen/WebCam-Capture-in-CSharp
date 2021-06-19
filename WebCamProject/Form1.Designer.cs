774a4f9212d430f6babff56f283a0670af884300
yuhanyen <digitalent.service@gmail.com>
2021-06-20 00:27:32 +0800

    WebCam Capture by C#

---
+++ WebCamProject/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WebCamProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picWebCam = new System.Windows.Forms.PictureBox();
+            this.picSnap = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lstDevices = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picWebCam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSnap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picWebCam
+            // 
+            this.picWebCam.Location = new System.Drawing.Point(12, 8);
+            this.picWebCam.Name = "picWebCam";
+            this.picWebCam.Size = new System.Drawing.Size(320, 240);
+            this.picWebCam.TabIndex = 0;
+            this.picWebCam.TabStop = false;
+            // 
+            // picSnap
+            // 
+            this.picSnap.Location = new System.Drawing.Point(346, 8);
+            this.picSnap.Name = "picSnap";
+            this.picSnap.Size = new System.Drawing.Size(320, 240);
+            this.picSnap.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picSnap.TabIndex = 1;
+            this.picSnap.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 256);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 88);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 88);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(397, 256);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 88);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lstDevices
+            // 
+            this.lstDevices.FormattingEnabled = true;
+            this.lstDevices.ItemHeight = 12;
+            this.lstDevices.Location = new System.Drawing.Point(12, 256);
+            this.lstDevices.Name = "lstDevices";
+            this.lstDevices.Size = new System.Drawing.Size(120, 88);
+            this.lstDevices.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 351);
+            this.Controls.Add(this.lstDevices);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picSnap);
+            this.Controls.Add(this.picWebCam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picWebCam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSnap)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picWebCam;
+        private System.Windows.Forms.PictureBox picSnap;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox lstDevices;
+    }
+}
+
---
+++ WebCamProject/WebCamera.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+namespace WebCamProject
+{
+    class WebCamera
+    {
+        private const int WM_CAP = 0x400;
+        private const int WM_CAP_DRIVER_CONNECT = 0x40a;
+        private const int WM_CAP_DRIVER_DISCONNECT = 0x40b;
+        private const int WM_CAP_EDIT_COPY = 0x41e;
+        private const int WM_CAP_SET_PREVIEW = 0x432;
+        private const int WM_CAP_SET_OVERLAY = 0x433;
+        private const int WM_CAP_SET_PREVIEWRATE = 0x434;
+        private const int WM_CAP_SET_SCALE = 0x435;
+        private const int WS_CHILD = 0x40000000;
+        private const int WS_VISIBLE = 0x10000000;
+
+        [DllImport("avicap32.dll")]
+        protected static extern bool capGetDriverDescriptionA(int wDriverIndex, [MarshalAs(UnmanagedType.VBByRefStr)]ref String ldevicename, int namelength, [MarshalAs(UnmanagedType.VBByRefStr)] ref String deviceversion, int versionlength);
+        [DllImport("avicap32.dll")]
+        protected static extern int capCreateCaptureWindowA([MarshalAs(UnmanagedType.VBByRefStr)] ref string windowname,
+            int style, int x, int y, int width, int height, int parent, int id);
+
+        [DllImport("user32", EntryPoint = "SendMessageA")]
+        protected static extern int SendMessage(int hwnd, int window, int x, [MarshalAs(UnmanagedType.AsAny)] object y);
+
+        int deviceHandle;
+
+
+        public static void LoadAllDevices(ListBox lstDevices)
+        {
+            String devicename = "".PadRight(100);
+            String deviceversion = "".PadRight(100);
+            lstDevices.Items.Clear();
+            for (int i = 0; i <= 10; i++)
+            {
+                bool isDeviceReady = capGetDriverDescriptionA(i, ref devicename, 100, ref deviceversion, 100);
+                if (!isDeviceReady)
+                    continue;
+                {
+                    WebCamera d = new WebCamera(devicename, deviceversion);
+
+
+                    lstDevices.Items.Add(d);
+                }
+            }
+
+
+        }
+
+
+        public System.Drawing.Image GetImage()
+        {
+            IDataObject data;
+            System.Drawing.Image snapshot;
+
+            SendMessage(deviceHandle, WM_CAP_EDIT_COPY, 0, 0);
+
+            data = Clipboard.GetDataObject();
+            if (data.GetDataPresent(typeof(System.Drawing.Bitmap)))
+            {
+                snapshot = (System.Drawing.Image)data.GetData(typeof(System.Drawing.Bitmap));
+                return snapshot;
+            }
+            return null;
+        }
+
+        string devicename, deviceversion;
+
+        public WebCamera(string devicename, string deviceversion)
+        {
+            this.devicename = devicename;
+            this.deviceversion = deviceversion;
+
+        }
+
+
+
+        public override string ToString()
+        {
+            return "Device Name = " + this.devicename + " , Device Version = " + this.deviceversion;
+        }
+
+        public void StartWebCam(int height, int width, int handle, int deviceno)
+        {
+            string deviceIndex = "" + deviceno;
+            deviceHandle = capCreateCaptureWindowA(ref deviceIndex, WS_VISIBLE | WS_CHILD, 0, 0, width, height, handle, 0);
+
+            if (SendMessage(deviceHandle, WM_CAP_DRIVER_CONNECT, deviceno, 0) > 0)
+            {
+                SendMessage(deviceHandle, WM_CAP_SET_SCALE, -1, 0);
+                SendMessage(deviceHandle, WM_CAP_SET_PREVIEWRATE, 0x42, 0);
+                SendMessage(deviceHandle, WM_CAP_SET_PREVIEW, -1, 0);
+            }
+        }
+
+        public void DisplayWebCam(PictureBox pic, int deviceno)
+        {
+            StartWebCam(pic.Height, pic.Width, (int)pic.Handle, deviceno);
+        }
+
+
+        public void StopWebCam(int deviceno)
+        {
+            SendMessage(deviceHandle, WM_CAP_DRIVER_DISCONNECT, deviceno, 0);
+        }
+
+    }
+}
